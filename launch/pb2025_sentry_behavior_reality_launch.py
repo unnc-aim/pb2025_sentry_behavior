c9e623583e972207d19d2cd0706e9b5fc78ab773
GentleLijie <zljzljsweepy@qq.com>
2026-03-30 19:26:26 +0800

    fix: add missing LaunchConfiguration import in launch file

--- launch/pb2025_sentry_behavior_reality_launch.py
+++ launch/pb2025_sentry_behavior_reality_launch.py
@@ -3,6 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node, PushRosNamespace, SetRemap
 from launch_ros.descriptions import ParameterFile
 from nav2_common.launch import RewrittenYaml
